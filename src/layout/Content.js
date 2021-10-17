ee84d2a1f0748e6b2d6fb0a838cfb72e4ab4b0b7
Buse Hizarci <bhzrc2705@gmail.com>
2021-10-17 12:11:48 +0100

    Content done

--- src/layout/Content.js
+++ src/layout/Content.js
@@ -1,11 +1,23 @@
-import React from 'react'
+import React , {useEffect} from 'react'
+import db from '../firebase;'
 
-const content = () => {
-    return (
-        <main className= "bg-purple-200 flex-1">
-            <h1>Content</h1>
-        </main>
-    )
-}
-
+return (
+    <main className="flex-1 flex flex-col border-r border-l border-gray-extraLight">
+      <header className="sticky top-0 z-10 bg-white flex justify-between items-center p-4 border-b border-gray-extraLight ">
+        <span className="font-bold text-xl text-gray-900">Home</span>
+        <PopulerIcon className="w-6 h-6 text-primary-base" />
+      </header>
+      <div className="flex space-x-4 px-4 py-3">
+        <img
+          src="https://pbs.twimg.com/profile_images/1373343596150132738/j8K08iHu_400x400.jpg"
+          alt="Profile"
+          className="w-11 h-11 rounded-full"
+        />
+        <TweetBox />
+      </div>
+      <Divider />
+      <FeedList tweets={tweets} />
+    </main>
+  );
+};
 export default content
